4d598ad787a10c5381c43e492491fe6c3d90a0c8
Steven Cherry <makotech222@gmail.com>
2016-05-30 12:18:35 -0500

    Implement QT GUI tab for changing keyboard controls

--- src/citra_qt/configure_input.h
+++ src/citra_qt/configure_input.h
@@ -1,10 +1,22 @@
-#ifndef CONFIGURE_INPUT_H
-#define CONFIGURE_INPUT_H
+// Copyright 2016 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+#pragma once
+#include <memory>
 
+#include <QKeyEvent>
+#include <QObject>
+#include <QPushButton>
+#include <QSettings>
+#include <QString>
 #include <QWidget>
 
+#include "citra_qt/config.h"
+#include "core/settings.h"
+#include "ui_configure_input.h"
+
 namespace Ui {
-class ConfigureInput;
+    class ConfigureInput;
 }
 
 class ConfigureInput : public QWidget
@@ -14,9 +26,20 @@ class ConfigureInput : public QWidget
 public:
     explicit ConfigureInput(QWidget *parent = 0);
     ~ConfigureInput();
+    void applyConfiguration();
 
 private:
-    Ui::ConfigureInput *ui;
-};
+    std::unique_ptr<Ui::ConfigureInput> ui;
+    std::map<Settings::NativeInput::Values, QPushButton*> input_mapping;
+    std::vector<int> keysPressed;
+    QPushButton* changingButton = NULL; // button currently waiting for key press.
 
-#endif // CONFIGURE_INPUT_H
+    void HandleClick();
+    void setConfiguration();
+    void SetKey();
+    void RemoveDuplicates(QString newValue);
+    void RestoreDefaults();
+    virtual void  keyPressEvent(QKeyEvent *event);
+    QString GetKeyName(int key_code);
+    int GetKeyValue(QString text);
+};
